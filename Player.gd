772ca006d25476998ab6f9dd0881aef86f551ebe
Sarumahr <56974823+Sarumahr@users.noreply.github.com>
2022-01-12 08:39:17 +0100

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -10,6 +10,8 @@ var jump = 5
 var cam_accel = 40
 var mouse_sense = 0.1
 var snap
+var current_weapon = 1
+var damage = 100
 
 #  vectors
 var direction = Vector3()
@@ -20,11 +22,36 @@ var movement = Vector3()
 # nodes
 onready var head = $Head
 onready var camera = $Head/Camera
+onready var gun1 = $Head/Hand/Gun1
+onready var gun2 = $Head/Hand/Gun2
+onready var gun3 = $Head/Hand/Gun3
+onready var aimcast = $Head/Camera/AimCast
+onready var muzzle = $Head/Gun/Muzzle
+onready var bullet = preload("res://scenes/Bullet.tscn")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _input(event):
+	#get mouse input for camera rotation
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sense))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				if current_weapon < 3:
+					current_weapon += 1
+				else:
+					current_weapon = 1
+			elif event.button_index == BUTTON_WHEEL_DOWN:
+				if current_weapon > 1:
+					current_weapon -= 1
+				else:
+					current_weapon = 3
+	
+func _process(delta):
 	# camera movement
 	if Engine.get_frames_per_second() > Engine.iterations_per_second:
 		camera.set_as_toplevel(true)
@@ -34,7 +61,7 @@ func _input(event):
 	else:
 		camera.set_as_toplevel(false)
 		camera.global_transform = head.global_transform
-		
+	
 func _physics_process(delta):
 	#get keyboard input
 	direction = Vector3.ZERO
@@ -42,6 +69,13 @@ func _physics_process(delta):
 	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	
+	if Input.is_action_just_pressed("fire"):
+		if aimcast.is_colliding():
+			var b = bullet.instance()
+			muzzle.add_child(b)
+			b.look_at(aimcast.get_collision_point(), Vector3.UP)
+			b.shoot = true
 	
 	#jumping and gravity
 	if is_on_floor():
@@ -62,3 +96,30 @@ func _physics_process(delta):
 	movement = velocity + gravity_vec
 	
 	move_and_slide_with_snap(movement, snap, Vector3.UP)
+
+func weapon_select():
+	
+	if Input.is_action_just_pressed("weapon1"):
+		current_weapon = 1
+	elif Input.is_action_just_pressed("weapon2"):
+		current_weapon = 2
+	elif Input.is_action_just_pressed("weapon3"):
+		current_weapon = 3
+		
+	if current_weapon == 1:
+		gun1.visible = true
+		gun1.shoot()
+	else:
+		gun1.visible = false
+
+	if current_weapon == 2:
+		gun2.visible = true
+		gun2.shoot()
+	else:
+		gun2.visible = false
+
+	if current_weapon == 3:
+		gun3.visible = true
+		gun3.shoot()
+	else:
+		gun3.visible = false
